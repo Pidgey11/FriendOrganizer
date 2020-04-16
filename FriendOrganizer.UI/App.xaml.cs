e774ed7381f776c359ccd9e4d0e80cb4ad8e059f
Pidgey11 <matisp1996@gmail.com>
2020-04-16 18:25:28 +0200

    added basic UI for a example data

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Startup;
+using FriendOrganizer.UI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +12,15 @@ using System.Windows;
 
 namespace FriendOrganizer.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootsrapper = new Bootstrapper();
+            var container = bootsrapper.Bootstrap();
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
---
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Startup
+{
+   public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+            
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+            return builder.Build();
+
+        }
+    }
+}
