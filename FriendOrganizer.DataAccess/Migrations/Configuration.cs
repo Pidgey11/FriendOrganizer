629bbf2833039c470730456067d2e630f0497abb
Pidgey11 <matisp1996@gmail.com>
2020-04-20 20:35:46 +0200

    added database

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿namespace FriendOrganizer.DataAccess.Migrations
+{
+    using FriendOrganizer.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Friend { FirstName = "Thomas", LastName = "Huber" },
+                new Friend { FirstName = "Urs", LastName = "Huber" },
+                new Friend { FirstName = "Erkan", LastName = "Egin" },
+                new Friend { FirstName = "Sara", LastName = "Huber" });
+        }
+    }
+}
