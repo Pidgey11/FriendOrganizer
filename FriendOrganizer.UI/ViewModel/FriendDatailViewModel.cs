6b2422d7c88757a976d35989025ce53bb919599b
Pidgey11 <matisp1996@gmail.com>
2020-04-29 12:35:36 +0200

    Extended user input validation

--- FriendOrganizer.UI/ViewModel/FriendDatailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDatailViewModel.cs
@@ -33,6 +33,14 @@ namespace FriendOrganizer.UI.ViewModel
             var friend = await _dataservice.GetByIdAsync(friendId);
 
             Friend = new FriendWrapper(friend);
+            Friend.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(Friend.HasErrors))
+                {
+                    ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+                }
+            };
+            ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
         }
 
         public FriendWrapper Friend
@@ -60,8 +68,8 @@ namespace FriendOrganizer.UI.ViewModel
 
         private bool OnSaveCanExecute()
         {
-            //TODO: Check if friend is valid
-            return true;
+            //TODO: Check in addition if friend has changes
+            return Friend != null && !Friend.HasErrors ;
         }
 
         private async void OnOpenFriendDatailView(int friendId)
